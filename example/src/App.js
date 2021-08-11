b9a7063f8cadc1087d19d866d2d3bc9c1e65a204
ZJVieth <52859000+ZJVieth@users.noreply.github.com>
2021-08-11 12:03:23 +0200

    V1 complete

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-device-manager'
-import 'react-device-manager/dist/index.css'
+import DeviceOption, { useDevice } from 'react-device-manager'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  let device = useDevice()
+
+  return (
+    <>
+      <p>{device.current}</p>
+
+      <DeviceOption device='mobile'>
+        <p>This only gets rendered for mobile.</p>
+      </DeviceOption>
+
+      <DeviceOption
+        device='tablet'
+        component={<p>This only gets rendered for tablets.</p>}
+      />
+
+      <DeviceOption device='desktop'>
+        This only gets rendered for desktop.
+      </DeviceOption>
+
+      <p>This always gets rendered!</p>
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,64 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+// Settings -------------------------------------------
+const default_setting = {
+  'mobile': [0, 600], // 360 x 414
+  'tablet': [601, 1280],
+  'desktop': [1280, 9999] // 1024 x 1920
+}
+
+
+// Utility ----------------------------------------------
+const getDevice = setting => {
+  let device
+
+  for (let entry of Object.entries(setting)) {
+    let name = entry[0]
+    let min = entry[1][0]
+    let max = entry[1][1]
+
+    device = name
+
+    let width = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth
+
+    if (width >= min && width <= max)
+      break
+  }
+
+  return device
+}
+
+// Hook -------------------------------------------------
+export function useDevice(custom_setting) {
+
+  const [device, setDevice] = useState(getDevice(custom_setting || default_setting))
+
+  useEffect(() => {
+
+    const updateDevice = () => {
+      setDevice(getDevice(custom_setting || default_setting))
+    }
+
+    window.addEventListener('resize', updateDevice)
+
+    return () => {
+      window.removeEventListener('resize', updateDevice)
+    }
+  }, [])
+
+  return { current: device }
+}
+
+
+// Renderer ----------------------------------------------
+export default function DeviceOption({ children, device, component }) {
+  if (device !== useDevice().current)
+    return null
+
+  if (component)
+    return component
+
+  return <React.Fragment>{children}</React.Fragment>
 }
